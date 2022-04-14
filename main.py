5f57dff670ee610d8e0daf2923355c686f7320e9
Griffin Brooks <80428506+FatherGriffin@users.noreply.github.com>
2022-04-13 19:49:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from discog import discog
 
 user = 'griffinbrooks47@gmail.com'
-password = 'XXX'
+password = '$Cornelius632'
 
 #artist_name = input("\nWhat artist discography do you want to use? ")
 #print("\nYou entered: " + artist_name)
 
-    #instantiates user specified discography
-d1 = discog(user, password)
-d1.search()
+#instantiates user specified discography
+d1 = discog(user, password)  
+d1.spotify_login()
+
