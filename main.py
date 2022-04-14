bd53f4ba61a61a7299c0228768d2bc6e71c291b2
Griffin Brooks <80428506+FatherGriffin@users.noreply.github.com>
2022-04-13 23:05:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from kivy.app import App
 from discog import discog
 
 user = 'griffinbrooks47@gmail.com'
@@ -7,6 +8,10 @@ password = '$Cornelius632'
 #print("\nYou entered: " + artist_name)
 
 #instantiates user specified discography
-d1 = discog(user, password)  
-d1.spotify_login()
+#d1 = discog(user, password)  
+#d1.spotify_login()
 
+class DiscogRankerApp(App):
+    pass
+
+DiscogRankerApp().run()
