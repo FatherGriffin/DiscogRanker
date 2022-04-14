1d1c2c4075b43e026df2315d75f22b8e989831fe
Griffin Brooks <80428506+FatherGriffin@users.noreply.github.com>
2022-04-14 17:35:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
+from threading import Thread
 from kivy.app import App
+from kivy.config import Config
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition, CardTransition
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.gridlayout import GridLayout
+
 from discog import discog
 
-user = 'griffinbrooks47@gmail.com'
-password = '$Cornelius632'
+Config.set('graphics', 'width', '1024')
+Config.set('graphics', 'height', '600')
 
-#artist_name = input("\nWhat artist discography do you want to use? ")
-#print("\nYou entered: " + artist_name)
+class MainGrid(Screen):
 
-#instantiates user specified discography
-#d1 = discog(user, password)  
-#d1.spotify_login()
+    def middle_click(self):
+        user = 'griffinbrooks47@gmail.com'
+        password = '$Cornelius632'
+        t = Thread(target=self.collect_discog(), args=(user, password))
+        t.daemon = True
+        # start the thread
+        t.start()
+
+    def collect_discog(user, password):
+
+        print("collecting discog...")
+        d1 = discog(user, password, 'Lil Tecca')  
+        d1.spotify_login()
+        d1.search()
 
 class DiscogRankerApp(App):
-    pass
+    
+    def build(self):
+
+        root = MainGrid()
+        return root
 
 DiscogRankerApp().run()
